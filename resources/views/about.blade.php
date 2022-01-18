fdc7aba2eb28ec01b8c1662e6b6cb377d506d4ed
ACER <hamidskj123@gmail.como>
2022-01-18 13:03:05 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hamid Sabirin</title>
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <img src="/images/2.jpg" alt="hamid">
+    <h2>{{$nama}}</h2>
+    <h2>{{$kelas}}</h2>
+    <h2>{{$alamat}}</h2>
+    
+</body>
+</html>
